adfdcf3594c2e11187ff4689b74b11c398508258
Holly <162161116+Holl4444@users.noreply.github.com>
2024-11-16 20:41:32 +0000

    Add button text and event listener
    Added text for visibility and use.
    Added prompt to eventlistener to take user input and
    check validity.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const boxes = document.getElementsByClassName("column");
+const popupBtn = document.getElementById("popup-btn");
 
 for (box = 0; box < boxes.length; box++) {
   boxes[box].addEventListener("mouseenter", (e) => {
@@ -7,3 +8,12 @@ for (box = 0; box < boxes.length; box++) {
     }
   });
 }
+
+popupBtn.addEventListener("click", () => {
+  const gridSize = parseInt(
+    prompt("Please enter the length of your grid (100 boxes max)", "4")
+  );
+  if (!gridSize || isNaN(gridSize)) {
+    alert("Invalid length");
+  }
+});
