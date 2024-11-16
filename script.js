8d5f51b3c07a429843728431ddd7129c20221e55
Holly <162161116+Holl4444@users.noreply.github.com>
2024-11-16 22:31:54 +0000

    Create function to make grid from input
    Called function inside eventlistener for readability and
    moved listener to bottom of script as I've seen elsewhere.

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ for (box = 0; box < boxes.length; box++) {
   });
 }
 
-popupBtn.addEventListener("click", () => {
-  const gridSize = parseInt(
-    prompt("Please enter the length of your grid (100 boxes max)")
-  );
-  if (!gridSize || isNaN(gridSize) || gridSize > 100 || gridSize < 1) {
-    alert("Invalid length");
+const createGrid = (size) => {
+  const grid = document.querySelector("section");
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < size; j++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
+    }
+    grid.appendChild(row);
   }
-});
+};
+
+/* delete initial grid
+ */
+
+popupBtn.addEventListener("click", () => {
+    const gridSize = parseInt(
+      prompt("Please enter the length of your grid (100 boxes max)")
+    );
+    if (!gridSize || isNaN(gridSize) || gridSize > 100 || gridSize < 1) {
+      alert("Invalid length");
+    }
+    createGrid(gridSize);
+  });
