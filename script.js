1b818b031112ae916416e0810ac0c2616c46ec05
Holly <162161116+Holl4444@users.noreply.github.com>
2024-11-17 14:43:30 +0000

    Add box attributes during creation.
    Set the grid to fully fill the same space by setting
    min width/height during column creation.
    
    Then set initial grid to onload process so we could
    reuse the code and not need more css.
    
    (Also removed TODO comment for this action.)

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 /* TODO: 
 
-Make grid and button consistent size by giving container dimensions
-and using borders instead of gaps?
-
 Style button
 
 Figure out how to make it run faster
@@ -13,6 +10,10 @@ Add way to toggle individual cells...button also pressed? click?
 
 const boxes = document.getElementsByClassName("column");
 const popupBtn = document.getElementById("popup-btn");
+
+window.onload = () => {
+  createGrid(4);
+};
 
 for (box = 0; box < boxes.length; box++) {
   boxes[box].addEventListener("mouseenter", (e) => {
@@ -33,15 +34,16 @@ const getGridSize = () => {
 };
 
 const createGrid = (size) => {
+  let attribute = `min-height: ${80 / size}vh; min-width: ${80 / size}vw;`;
   const grid = document.querySelector("section");
   grid.replaceChildren();
-  //grid.innerHTML = "";
   for (let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     for (let j = 0; j < size; j++) {
       const column = document.createElement("div");
       column.classList.add("column");
+      column.setAttribute("style", attribute);
       column.addEventListener("mouseenter", (e) => {
         {
           e.target.classList.toggle("draw");
